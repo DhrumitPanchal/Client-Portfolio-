423041d8038a4056802729df2b9d9da45e4145f1
dhrumit panchal <dhrumitpanchal789@gmail.com>
2023-10-27 12:47:28 +0530

    finished about section

--- src/pages/Component/Navbar.js
+++ src/pages/Component/Navbar.js
@@ -8,7 +8,7 @@ const [openmenu,setOpenmenu] = useState(false)
 
   return (
   
-   <div className='px-[2rem] py-[.4rem] fixed flex justify-between items-center z-50 bg-white text-[#041A17] w-full shadow-[0_5px_10px_rgba(0,0,0,0.2)]'>
+   <div className='px-[2rem] py-[.4rem] absolute flex justify-between items-center z-50 bg-white text-[#041A17] w-full shadow-[0_5px_10px_rgba(0,0,0,0.2)]'>
       <h2 className='logo cursor-pointer text-[1.8rem] font-bold '><span className='text-[#1c31b5]'>V</span>ishwas</h2>
       <ul className='flex justify-center items-center gap-[1.2rem] text-[1.2rem] font-medium capitalize'>
         <a href='#Home' className='cursor-pointer hover:text-[#1c31b5] transition-colors duration-300'>home</a>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -55,7 +55,78 @@ function LandingPage() {
           </a>
         </div>
       </section>
-     
+      <section
+        id="About"
+        className="px-[4rem]  py-[2rem] flex  items-center flex-col gap-[.6rem] h-screen max-sm:h-full w-full text-black"
+      >
+        <div className="flex flex-col items-center">
+          <h2 className="title text-[2rem] font-semibold uppercase">
+            ABOUT ME
+          </h2>
+          <div className="mt-[.2rem] h-[.4rem] w-[8rem] bg-[#1c31b5] rounded-[.2rem]"></div>
+          <p className="mt-[.8rem] w-[50rem] text-[1.2rem] text-center  ">
+            Here you will find more information about me, what i do,and my
+            current skills mostly in terms of programming and technology.
+          </p>
+        </div>
+        <div className="mt-[6rem] h-full w-full flex justify-between items-center gap-[6rem]">
+          <div className="h-full w-3/5 px-[2rem]">
+            <div className="h-full flex flex-col gap-[1rem] pr-[4rem]">
+              <div>
+                <h2 className="title text-[1.7rem] font-semibold uppercase">
+                  Something about me
+                </h2>
+                <div className="mt-[.2rem] h-[.4rem] w-[8rem] bg-[#1c31b5] rounded-[.2rem]"></div>
+              </div>
+              <p className="mt-[1rem] text-[1.1rem] ">
+                I'm a <span className="font-semibold">Frontend Web Developer</span>  building the Front-end of Websites
+                and Web Applications that leads to the success of the overall
+                product. Check out some Of my work in the <span className="font-semibold">Projects</span> section.
+              </p>
+              <p className="mt-[rem] text-[1.1rem] ">
+                {" "}
+                also like sharing content related to the stuff that I have
+                learned over the years in Web Development so it can help other
+                people of the Dev Community. Feel free to Connect or Follow me
+                on my <span className="font-semibold">LinkedIn</span> where I post useful content related to Web
+                Development and Programming
+              </p>
+              <p className="mt-[rem] text-[1.1rem] ">
+                I'm open to <span className="font-semibold">Job</span> opportunities where I can contribute, learn and
+                grow. If you have a good opportunity that matches my skills and
+                experience then don't hesitate to <span className="font-semibold">contact</span> me.
+              </p>
+              <button className="uppercase mt-[.6rem] flex justify-center items-center  h-[2.5rem] w-[7rem] rounded-[.4rem] text-[1rem]  font-semibold text-white bg-[#1c31b5]">
+                Contact
+              </button>
+            </div>
+          </div>
+
+          <div className="h-full w-2/5">
+            <div className="flex flex-col ">
+              <h2 className="title text-[1.7rem] font-semibold uppercase">
+                Skills that i own
+              </h2>
+              <div className="mt-[.2rem] h-[.4rem] w-[8rem] bg-[#1c31b5] rounded-[.2rem]"></div>
+            </div>
+            <div className="mt-[1.6rem] h-[22rem] w-[28rem] grid grid-rows-6 grid-cols-9 gap-4">
+                <div  className="flex justify-center items-center text-[2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-3 row-span-2 text-[#666666] font-medium">HTML</div>
+                <div className="flex justify-center items-center text-[1.2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-2 text-[#666666] font-medium">CSS</div>
+                <div className="flex justify-center items-center text-[1.2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-2 text-[#666666] font-medium">SQL</div>
+                <div className="flex justify-center items-center text-[1.2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] row-span-2 col-span-2 text-[#666666] font-medium ho">S <br />E <br />O</div>
+                <div className="flex justify-center items-center text-[1.2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-4 text-[#666666] font-medium">JavaScript</div>
+                <div className="flex justify-center items-center text-[2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-4 row-span-2 text-[#666666] font-medium">Kotlin</div>
+                <div className="flex justify-center items-center text-[1.3rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-3 row-span-2 text-center text-[#666666] font-medium">Responsive <br />Design</div>
+                <div className="flex justify-center items-center text-[1.2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem]  row-span-2 col-span-2 text-center text-[#666666] font-extralight">Git <br />&<br /> GitHub</div>
+                <div className="flex justify-center items-center text-[1.2rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-2 text-[#666666] font-extralight">SASS</div>
+                <div className="flex justify-center items-center text-[1.6rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-3 text-[#666666] font-medium">Java</div>
+                <div className="flex justify-center items-center text-[1.6rem] h-full w-full bg-[#e7e7e7] rounded-[.5rem] col-span-4 text-[#666666] font-medium">Node</div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
     </>
   );
 }
